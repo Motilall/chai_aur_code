421e122a44597e2417468c46ac999047d519da4a
Motilall <moitlalkayam@gmai.com>
2024-07-13 23:41:35 +0530

    .

--- 30_days_js_challenge/day_1/script.js
+++ 30_days_js_challenge/day_1/script.js
@@ -2,8 +2,42 @@
 
 // task 1
 var a = 12;
-console.log(a);
+//console.log(a);
 
 // task 2
 let b = "red_pearl";
-console.log(b);
+//console.log(b);
+
+
+
+
+
+// ++++++++++++++++++++ Activity 2 ++++++++++++++++++++++++++++++
+
+// task 1
+const c = true;
+//console.log(c);
+
+
+
+
+
+
+
+// +++++++++++++++++++ Activity 3 +++++++++++++++++++++++++++++++++
+
+// task 1
+let d = 12;
+let e = "red";
+let f = true;
+const person = {
+    name: 'red',
+    age: 20
+};
+let arr = [1,2,3,4,5];
+
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log(typeof(person));
+console.log(typeof(arr));
