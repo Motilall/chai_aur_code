ca78943c265db7052286d2629ff21fc831defa15
Motilall <moitlalkayam@gmai.com>
2024-07-14 10:33:46 +0530

    .

---
+++ 30_days_js_challenge/day_2/script.js
@@ -0,0 +1,103 @@
+// +++++++++++++++++++++++ Activity 1 ++++++++++++++++++++++++++++
+
+// task 1
+let a = 2;
+let b = 2;
+let add = a+b;
+// console.log("The addtion of " + a + " and " + b + " is " + add);
+
+// task 2
+let subtract = a-b;
+// console.log("The subtraction of " + a + " and " + b + " is " + subtract);
+
+// task 3
+let multiply = a*b;
+// console.log("The multiplication of " + a + " and " + b + " is " + multiply);
+
+// task 4
+let divide = a/b;
+// console.log("The division of " + a + " and " + b + " is " + divide);
+
+// task 5 
+let remainder = a%b;
+// console.log("The remainder of " + a + " and " + b + " is " + remainder);
+
+
+
+
+
+// ++++++++++++++++++++++ Activity 2 ++++++++++++++++++++++++++++
+
+// task 6
+let c = 5
+c += 3 ;
+// console.log(c);
+
+// task 7
+let d = 7;
+d -= 3;
+// console.log(d);
+
+
+
+
+
+// ++++++++++++++++++++++ Activity 3 ++++++++++++++++++++++++++++++++
+
+// task 8
+let e = 7 > 2;
+// console.log(e);
+let f = 7 < 2;
+// console.log(f);
+
+// task 9
+let g = 7 >= 2;
+// console.log(g);
+let h = 7 <= 2;
+// console.log(h);
+
+// task 10 
+let i = 7 == '7';// it is checking the type 
+// console.log(i);
+let j = 7 === '7';// it is checking the value
+// console.log(j);
+
+
+
+
+
+
+// +++++++++++++++++++++++ Activity 4 ++++++++++++++++++++++++++
+
+// task 11 
+let m = 7 > 5 && 5 > 3;
+// console.log(m);// both should be true
+
+// task 12
+let n = 7 > 5 || 3 > 5;
+// console.log(n);// only one should be correct 
+
+// task 13
+let isTrue = true;
+console.log(!isTrue);// false
+
+
+
+
+
+
+
+// ++++++++++++++++++++++ Activity 5 +++++++++++++++++++++++++
+
+// task 14 
+let age = 14;
+let canVote = age > 18 ? 'vote' : 'can not vote';
+console.log(canVote);
+
+let attendence = 92;
+let percetnage = attendence > 75 ? "enter the exam hall" : 'Cannot eneter';
+console.log(percetnage);
+
+let num = 7;
+let result = num >= 0 ? "postive" : " negetive";
+console.log(result);
