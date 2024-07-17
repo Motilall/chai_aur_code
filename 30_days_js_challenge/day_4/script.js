8d22d0008250c826f1ca3c62c73d0c72214ac03b
Motilall <moitlalkayam@gmai.com>
2024-07-17 19:48:44 +0530

    .

---
+++ 30_days_js_challenge/day_4/script.js
@@ -0,0 +1,110 @@
+// +++++++++++++++++++++++ Activity 1 ++++++++++++++++++++++++++
+
+// task 1
+// let i;
+// for(i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+// task 2
+
+// let i,j;
+// for(i = 1; i <= 10; i++){
+//     j = 5 * i;
+//     console.log("5 * " + i + " = " + j);
+// };
+
+
+
+
+
+
+
+
+
+// +++++++++++++++++++++++ Activity 2 ++++++++++++++++++++++++++
+
+// task 3
+
+// let i = 1, sum = 0;
+// while(i <= 10){
+//     j = i;
+//     sum += j;
+//     i++;
+// }
+// console.log(sum);
+
+// task 4
+
+// let i = 10;
+// while(i>0){
+//     console.log(i);
+//     i--;
+// }
+
+// +++++++++++++++++++++++ Activity 2 ++++++++++++++++++++++++++++
+
+// task 5
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 5);
+
+// task 6
+
+// let i = 1, factorial = 1;
+// do{
+//     factorial *= i;
+//     i++;
+// }while(i <= 5);
+// console.log(factorial);
+
+
+
+
+
+
+
+// +++++++++++++++++++++++++ Activity 4 ++++++++++++++++++++++++++++++++
+
+// task 7 
+
+// let i,j;
+// for(i = 1; i <= 5; i++){
+//     let row = "";
+//     for(j = 1; j <= i; j++){
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+
+
+
+
+
+
+
+// +++++++++++++++++++++++++ Activity 5 +++++++++++++++++++++++++
+
+// task 8 
+
+// let i;
+// for(i = 1; i <= 10; i++){
+//     if(i === 5){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// task 9
+
+let i;
+for(i = 1; i <= 10; i++){
+    if(i === 7){
+        break;
+    }
+    console.log(i);
+}
